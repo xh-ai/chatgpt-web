dc6ddcbd4cbce22299761eb6b43feca8e1ce4c0f
dreamlx <dreamlinx@gmail.com>
2023-05-31 10:49:14 +0800

    fix

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -4,6 +4,12 @@ import fetch from 'node-fetch'
 const auth = async (req, res, next) => {
   const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
   const chat_lm_uri = process.env.CHAT_LM_URI || ''
+  // 将用户名和密码编码为Base64字符串
+  const auth_code = Buffer.from('ck_19465cb4cb67a9649058b60d7e78168059bcd818:cs_f20060b646e2ce1dc395d60290bb2a874cd6993b').toString('base64')
+  // 添加Basic Auth认证信息
+  const headers = {
+    Authorization: `Basic ${auth_code}`,
+  }
 
   if (isNotEmptyString(AUTH_SECRET_KEY)) {
     try {
